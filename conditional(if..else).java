fe2a68960e331b44c02c0ca7a805403771ad1644
Fahad Masroor <119754253+fahadi04@users.noreply.github.com>
2023-03-07 01:25:46 +0530

    Create conditional(if..else).java

---
+++ conditional(if..else).java
@@ -0,0 +1,15 @@
+# JAVAPROGRAM
+import java.util.Scanner;
+class main(){
+  public static void main(){
+    System.out.println("Enter the value");
+    Scanner Sc =new Sc(System.in);
+    int a = Sc.nextInt();
+    int b = Sc.nextInt();
+    if(a>b){
+      System.out.println("True");
+    }else{
+        System.out.println("False");
+    }
+  }
+}
