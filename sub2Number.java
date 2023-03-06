26b6eab95f5c8f81c9f2c4031caf60bd6c363ea3
Fahad Masroor <119754253+fahadi04@users.noreply.github.com>
2023-03-07 01:23:51 +0530

    Create sub2Number.java

---
+++ sub2Number.java
@@ -0,0 +1,14 @@
+# JAVAPROGRAM
+import java.util.Scanner;
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Enter the value a,b");
+  Scanner Sc = new Scanner(System.in);
+  int a = Sc.nextInt();
+  int b = Sc.nextInt();
+  int sum= a+b ;
+    System.out.println(sum);
+  }
+}
