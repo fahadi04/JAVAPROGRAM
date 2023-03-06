c3af192e563f72d3f285353b0b1b5c0021f515fb
Fahad Masroor <119754253+fahadi04@users.noreply.github.com>
2023-03-07 01:22:53 +0530

    Create sum2Number.java

---
+++ sum2Number.java
@@ -0,0 +1,13 @@
+# JAVAPROGRAMimport java.util.Scanner;
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Enter the value a,b");
+  Scanner Sc = new Scanner(System.in);
+  int a = Sc.nextInt();
+  int b = Sc.nextInt();
+  int sum= a+b ;
+    System.out.println(sum);
+  }
+}
