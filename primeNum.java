156889eb7143fc9cf7d736a664ee0b80a990a9a6
Fahad Masroor <119754253+fahadi04@users.noreply.github.com>
2023-03-20 00:51:40 +0530

    Create primeNum.java

---
+++ primeNum.java
@@ -0,0 +1,16 @@
+public class primNum {
+    public static void main(String args[]){
+        int count;
+        for(int i=1;i<=100;i++){
+            count=0;
+            for(int j=1;j<=i;j++){
+                if(i%j==0){
+                    count++;
+                }
+            }
+            if(count==2){
+                System.out.println(i);
+            } 
+        }
+    }
+}
