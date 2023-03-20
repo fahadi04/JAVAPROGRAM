1f11deb0006b5dd8e66fa4598e0d471476a235fe
Fahad Masroor <119754253+fahadi04@users.noreply.github.com>
2023-03-21 01:34:40 +0530

    Create wideningtypeCasting.java

---
+++ wideningtypeCasting.java
@@ -0,0 +1,12 @@
+public class WideningtypeCasting {
+    public static void main(String[] args){
+        int myInt=10;
+        // double myDouble=myInt;   // Automatic casting
+        float myFloat=myInt;        //Automatic casting
+        System.out.println(myInt);  //outpur 10
+        // System.out.println(myDouble); //output 10.0
+        System.out.println(myFloat);   //output 10.0
+
+    }
+    
+}
