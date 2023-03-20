70f6325932a36e0956988cd533fd03f991e16dc7
Fahad Masroor <119754253+fahadi04@users.noreply.github.com>
2023-03-21 01:33:47 +0530

    Create NarrowingtypeCasting.java

---
+++ NarrowingtypeCasting.java
@@ -0,0 +1,11 @@
+public class NarrowingtypeCasting {
+    public static void main(String args[]){
+        // double myNum=10.0d;
+        float myNum=10.0f;
+        int myInt=(int) myNum;       //Manual Casting
+        System.out.println(myNum);   //output 10.0
+        System.out.println(myInt);   //output 10
+
+    }
+    
+}
